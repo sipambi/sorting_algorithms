4d576790722f77e5293db61f064b736fa5b5e93c
sipambi <96469754+sipambi@users.noreply.github.com>
2022-07-19 22:48:36 +0200

    update

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,89 +1,75 @@
 #include "sort.h"
-#include <stdlib.h>
-#include <stdio.h>
 
 /**
- * Merge - sorts and merges the sub arrays in source
- * @start: starting index (inclusive) for the left sub array
- * @middle: end index (exclusive) for the left sub array and
- * starting index (inclusive) for the right sub array
- * @end: end index (exclusive) for the right sub array
- * @dest: destination for data
- * @source: source of data
- *
- * Return: nothing
+ * quick_sort - sorts an array with the Quicksort algorithm
+ * @array: array of ints to sort
+ * @size: size of the array
  */
-void Merge(size_t start, size_t middle, size_t end, int *dest, int *source)
+void quick_sort(int *array, size_t size)
 {
-	size_t i, j, k;
-
-	printf("Merging...\n");
-	printf("[left]: ");
-	print_array(source + start, middle - start);
-	printf("[right]: ");
-	print_array(source + middle, end - middle);
-	i = start;
-	j = middle;
-	for (k = start; k < end; k++)
-	{
-		if (i < middle && (j >= end || source[i] <= source[j]))
-		{
-			dest[k] = source[i];
-			i++;
-		}
-		else
-		{
-			dest[k] = source[j];
-			j++;
-		}
-	}
-	printf("[Done]: ");
-	print_array(dest + start, end - start);
-}
-
-/**
- * SplitMerge - recursively splits the array and merges the sorted arrays
- * @start: starting index (inclusive)
- * @end: end index (exclusive)
- * @array: the array to sort
- * @copy: a copy of the array
- *
- * Return: nothing
- */
-void SplitMerge(size_t start, size_t end, int *array, int *copy)
-{
-	size_t middle;
-
-	if (end - start < 2)
+	if (size < 2)
 		return;
-	middle = (start + end) / 2;
-	SplitMerge(start, middle, array, copy);
-	SplitMerge(middle, end, array, copy);
-	Merge(start, middle, end, array, copy);
-	for (middle = start; middle < end; middle++)
-		copy[middle] = array[middle];
+
+	quick_recursion(array, 0, (int)size - 1, size);
 }
 
 /**
- * merge_sort - implements Merge sort algorithm & sorts an array of integers
- *              in ascending order
+ * quick_recursion - helper function for Quicksort
  * @array: array to sort
+ * @left: index of the left element
+ * @right: index of the right element
+ * @size: size of the array
+ */
+void quick_recursion(int *array, int left, int right, size_t size)
+{
+	int piv;
+
+	if (left < right)
+	{
+		piv = partition(array, left, right, size);
+		quick_recursion(array, left, piv - 1, size);
+		quick_recursion(array, piv + 1, right, size);
+	}
+}
+
+/**
+ * partition - gives a piv index for Quicksort
+ * @array: array to find the piv in
+ * @left: index of the left element
+ * @right: index of the right element
  * @size: size of the array
  *
- * Return: nothing
+ * Return: the index of the piv element
  */
-void merge_sort(int *array, size_t size)
+int partition(int *array, int left, int right, size_t size)
 {
-	size_t i;
-	int *copy;
+	int tmp, i;
+	int j;
 
-	if (array == NULL || size < 2)
-		return;
-	copy = malloc(sizeof(int) * size);
-	if (copy == NULL)
-		return;
-	for (i = 0; i < size; i++)
-		copy[i] = array[i];
-	SplitMerge(0, size, array, copy);
-	free(copy);
+	i = left - 1;
+
+	for (j = left; j < right; j++)
+	{
+		if (array[j] < array[right])
+		{
+			i++;
+			if (i != j)
+			{
+				tmp = array[i];
+				array[i] = array[j];
+				array[j] = tmp;
+				print_array(array, size);
+			}
+		}
+	}
+
+	if (array[right] < array[i + 1])
+	{
+		tmp = array[i + 1];
+		array[i + 1] = array[right];
+		array[right] = tmp;
+		print_array(array, size);
+	}
+
+	return (i + 1);
 }
