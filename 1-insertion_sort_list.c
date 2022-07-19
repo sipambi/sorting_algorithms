7a0f070a90705f48711c4e8bf8bdc18ca2672fe6
sipambi <96469754+sipambi@users.noreply.github.com>
2022-07-19 22:35:10 +0200

    edited

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,35 +1,61 @@
 #include "sort.h"
+
 /**
- * insertion_sort_list - implements Insertion sort algorithm and it
- * works on a doubly linked list of integers to sort it in ascending
- * order
- * @list: Double pointer to the head of the linked list
- * Return: nothing
+ * len_list - returns the length of a linked list
+ * @h: pointer to the list
+ *
+ * Return: length of list
+ */
+int len_list(listint_t *h)
+{
+	int len = 0;
+
+	while (h)
+	{
+		len++;
+		h = h->next;
+	}
+	return (len);
+}
+
+/**
+ * insertion_sort_list - sorts a linked list with the Insert Sort algorithm
+ * @list: double pointer to the list to sort
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *temp, *temp2;
+	listint_t *curr = NULL, *one = NULL;
+	listint_t *two = NULL, *three = NULL, *four = NULL;
 
-	if (list == NULL || *list == NULL)
+	if (!list || !(*list) || len_list(*list) < 2)
 		return;
-	temp = (*list)->next;
-	while (temp != NULL)
+
+	curr = *list;
+
+	while (curr)
 	{
-		temp2 = temp->next;
-		while (temp->prev != NULL && temp->prev->n > temp->n)
+		if (curr->prev && curr->n < curr->prev->n)
 		{
-			temp->prev->next = temp->next;
-			if (temp->next != NULL)
-				temp->next->prev = temp->prev;
-			temp->next = temp->prev;
-			temp->prev = temp->next->prev;
-			temp->next->prev = temp;
-			if (temp->prev == NULL)
-				*list = temp;
+			one = curr->prev->prev;
+			two = curr->prev;
+			three = curr;
+			four = curr->next;
+
+			two->next = four;
+			if (four)
+				four->prev = two;
+			three->next = two;
+			three->prev = one;
+			if (one)
+				one->next = three;
 			else
-				temp->prev->next = temp;
+				*list = three;
+			two->prev = three;
+			curr = *list;
 			print_list(*list);
+			continue;
 		}
-		temp = temp2;
+		else
+			curr = curr->next;
 	}
 }
