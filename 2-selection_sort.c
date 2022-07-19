df044cb14723b8e47b5cdb278bcfcf0c89f20d0f
sipambi <96469754+sipambi@users.noreply.github.com>
2022-07-19 21:34:53 +0200

    2 Commit

---
+++ 2-selection_sort.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+
+/**
+ * selection_sort - implements selection sorts algorithm and it sorts
+ *                  in ascending order
+ * @array: The array to sort
+ * @size: The size of the array
+ *
+ * Return: nothing
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, min;
+	int temp;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min])
+				min = j;
+		}
+		if (i != min)
+		{
+			temp = array[i];
+			array[i] = array[min];
+			array[min] = temp;
+			print_array(array, size);
+		}
+	}
+}
